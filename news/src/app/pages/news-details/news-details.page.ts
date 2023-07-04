c252cc819c46eeca4d7f5813eb2d4e37e12305ee
Aaron Lawlor <aaronjlawlor@yahoo.ie>
2023-07-04 20:40:10 +0000

    commit fonts

--- news/src/app/pages/news-details/news-details.page.ts
+++ news/src/app/pages/news-details/news-details.page.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 @Component({
-  selector: 'app-news-details',
-  templateUrl: './news-details.page.html',
-  styleUrls: ['./news-details.page.scss'],
+	selector: 'app-news-details',
+	templateUrl: './news-details.page.html',
+	styleUrls: ['./news-details.page.scss'],
 })
 export class NewsDetailsPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+	news: any;
+	constructor(private activatedRoute: ActivatedRoute, private http: HttpClient) { }
+	ngOnInit() {
+		let id = this.activatedRoute.snapshot.paramMap.get('id');
+		this.http.get(`https://swapi.dev/api/news/${id}`).subscribe(res => {
+			this.news = res;
+		});
 }
+}
+
--- news/src/app/pages/news/news.page.ts
+++ news/src/app/pages/news/news.page.ts
@@ -1,42 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
-import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-news',
-  templateUrl: './news.page.html',
-  styleUrls: ['./news.page.scss'],
+	selector: 'app-news',
+	templateUrl: './news.page.html',
+	styleUrls: ['./news.page.scss'],
 })
+
 export class NewsPage implements OnInit {
-  news: any; // Updated type to 'any'
-
-  constructor(private router: Router, private http: HttpClient) { }
-
-  ngOnInit() {
-    const url = 'https://newsapi.org/v2/top-headlines?' +
-      'country=ie&' +
-      'apiKey=c55dc7fee88f4c21a87a37bba674415d';
-
-    // Optional: Add headers if necessary
-    const headers = new HttpHeaders({
-      'Content-Type': 'application/json'
-    });
-
-    this.http.get(url, { headers }).subscribe(
-      (data) => {
-        this.news = data;
-        console.log('my data:', this.news);
-      },
-      (error: HttpErrorResponse) => {
-        console.error('An error occurred:', error);
-      }
-    );
-  }
-
-  shownews(news: { url: string; }) {
-    let split = news.url.split('/');
-    let newsId = split[split.length - 2];
-    this.router.navigateByUrl(`/tabs/news/${newsId}`);
-  }
+	news: Observable<any> | undefined;
+	constructor(private router: Router, private http: HttpClient) { }
+	
+	ngOnInit() {
+		this.news = this.http.get('https://swapi.dev/api/films');
+	  }
+	  
+	  showNews(news: { url: string; }){
+		let split = news.url.split('/');
+		let newsId = split[split.length-2];
+		this.router.navigateByUrl(`/tabs/news/${newsId}`);
+	  }
 }
